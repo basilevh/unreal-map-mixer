604055c7e951981f083a60c1cbedcb6eac75051a
Basile VH <basile@outlook.be>
2018-07-01 16:29:15 +0200

    Added first tests for T3DFormatter

---
+++ UnrealMapMixerTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnrealMapMixerTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnrealMapMixerTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2cbda589-9212-42b0-af42-db468ccac2ca")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnrealMapMixerTests/Unreal/T3DFormatterTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnrealMapMixer.Unreal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnrealMapMixer.Unreal.Tests
+{
+    [TestClass()]
+    public class T3DFormatterTests
+    {
+        [TestMethod()]
+        public void GetVertexCoordTest()
+        {
+            Assert.AreEqual("+00000.000000", T3DFormatter.GetVertexCoord(0.0));
+            Assert.AreEqual("+00001.100000", T3DFormatter.GetVertexCoord(1.1));
+            Assert.AreEqual("-12345.123456", T3DFormatter.GetVertexCoord(-12345.123456));
+        }
+    }
+}
